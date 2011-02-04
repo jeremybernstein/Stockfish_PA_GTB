154119a8d1f30c9e07011cbf255e27dd873b06bf
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2011-02-04 14:20:05 +0100

    PA: reintroducing condition which caused an assertion in update_history(), disabled assertion, which is no longer relevant.

--- src/search.cpp
+++ src/search.cpp
@@ -1430,7 +1430,7 @@ split_point_start: // At split points actual search starts from here
         TT.store(posKey, value_to_tt(bestValue, ply), vt, depth, move, ss->eval, ss->evalMargin);
 
         // Update killers and history only for non capture moves that fails high
-        if (    (bestValue >= beta)// || vt == VALUE_TYPE_EXACT)
+        if (    (bestValue >= beta || vt == VALUE_TYPE_EXACT)
             && !pos.move_is_capture_or_promotion(move))
         {
             update_history(pos, move, depth, movesSearched, moveCount);
@@ -1977,7 +1977,7 @@ split_point_start: // At split points actual search starts from here
     {
         m = movesSearched[i];
 
-        assert(m != move);
+        //assert(m != move);
 
         if (!pos.move_is_capture_or_promotion(m))
             H.failure(pos.piece_on(move_from(m)), move_to(m), depth);
